c143a189c1e91d33321fc3eb1a8930213b64cc75
miratepuffin <piratemuffin@hotmail.co.uk>
2018-04-18 02:42:31 +0100

    Add successful posts to a list so the process can resume if crash or block

--- Gab-crawler.py
+++ Gab-crawler.py
@@ -1,10 +1,9 @@
-import psycopg2
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 import redis
 
 def getThyPage(postCount):
-    req = Request('https://gab.ai/posts/'+str(postCount), headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request('https://gab.ai/posts/'+str(postCount), headers={'User-Agent': 'Mozilla/5.0'}) #Pretend to be Mozilla
     webpage = urlopen(req).read()
     return str(webpage)
 
@@ -12,12 +11,18 @@ r = redis.Redis(
     host='localhost',
     port=6379)
 
+lastPost = r.lindex("gab-posts",0) #get the last post which was saved to redis
+if(lastPost==None):
+    lastPost=1
+else:
+    lastPost=int(lastPost)+1
 
-for x in range(1, 10):
+for x in range(lastPost, 30000000): #From the last post to roughly all posts on GAB
     try:
-        r.set(str(x), getThyPage(x))
-        print("Post "+str(x)+" Success")
-        #print(r.get(str(x)))
+         r.set(str(x), getThyPage(x)) #save the post as a string
+         r.lpush("gab-posts", x) #and add successful posts to the list to know where to continue from if we crash
+         print("Post "+str(x)+" Success")
+#        #print(r.get(str(x)))
     except HTTPError:
         pass
         #print("Post "+str(x)+" Unavailable")
